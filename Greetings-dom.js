d909fead4a13194b980a28892e03424deb125fbc
FidelDhlamini <goodkidfidel@gmail.com>
2019-07-19 14:54:10 +0200

    completed Greetings

--- Greetings-dom.js
+++ Greetings-dom.js
@@ -2,15 +2,26 @@ var userInput = document.querySelector(".nameInput");
 var greetBtn = document.querySelector(".greetButton");
 var display = document.querySelector(".greetingMessage");
 var radio = document.querySelector(".whichLanguage");
+var counter = document.querySelector(".nameCounter");
 var instance = Greetings();
 
 
 
 function greetMe() {
 
-    
-    var enter = instance.greet(userInput.value);
-    display.innerHTML = enter;
+    if (radio) {
+        var language = document.querySelector("input[name='languageName']:checked");
+        var greetingDisplay = instance.greet(userInput.value, language.value)
+        //   console.log(language.value)
+        //   console.log(userInput.value)
+        //        display = greetingDisplay.innerHTML;
+        //   console.log(display);
+
+    }
+    display.innerHTML = greetingDisplay;
+    counter.innerHTML = instance.countNames();
+
+
 
 }
 
--- greet.js
+++ greet.js
@@ -1,15 +1,39 @@
 var Greetings = function () {
-    let names = 0;
-    function greetMe(name) {
-        return "Hello " + name;
+    let counter = 0;
+    let names = {};
+    let final;
+
+    function greetMe(name, language) {
+
+        ///names = name;
+        // console.log(language);
+        if (language === "English") {
+            counter++;
+            console.log("Hello, " + name)
+            return "Hello, " + name;
+        }
+        if (language === "Xhosa") {
+            counter++;
+            return "Molo, " + name;
+        } else if (language === "Afrikaans") {
+            counter++;
+            return "Hallo, " + name;
+        }
+
     }
-    console.log(greetMe("Fidel"));
-    function counter(name){
-       name += names;
-        return names
+
+    function nameContainer() {
+        return names;
+
     }
-    
+
+    function getCount() {
+        return counter;
+    }
+
     return {
         greet: greetMe,
+        countNames: getCount
     }
+
 }
